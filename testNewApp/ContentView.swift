1cd90a0f0a6c6615239bd69f0a77c7b2f01a160c
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-02-16 11:39:04 +0200

    setting up the app

---
+++ testNewApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testNewApp
+//
+//  Created by asmaa gamal  on 16/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testNewApp/testNewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testNewAppApp.swift
+//  testNewApp
+//
+//  Created by asmaa gamal  on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct testNewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
